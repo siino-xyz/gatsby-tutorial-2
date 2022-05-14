455f44b0ef05d169bb7b3d9ccb5f552bf41056bb
siino-xyz <b114807h@gmail.com>
2022-05-15 02:17:28 +0900

    Finished Gatsby Tutorial Part 6

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Layout from "../../components/layout";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import { graphql } from "gatsby";
+
+const BlogPost = ({ data }) => {
+  return (
+    <Layout pageTitle="Super Cool Blog Posts">
+      <p>data.mdx.frontmatter.date</p>
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`;
+
+export default BlogPost;
